bfee2a2be80cf6141cf1bafccf74cc686296dca9
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-04-07 18:21:11 -0500

    Added SystemBus.cs

---
+++ Assignment4/CIS726_Assignment2/SystemBus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Messaging;
+
+namespace CIS726_Assignment2
+{
+    public class SystemBus
+    {
+        private MessageQueue _queue;
+
+        public SystemBus(string queueName)
+        {
+            _queue = new MessageQueue(queueName);
+        }
+
+        public void SendMessage(string label, string message)
+        {
+            Message msg = new Message();
+            msg.Body = message;
+            msg.Label = label;
+            _queue.Send(msg);
+        }
+
+        public string CheckMessage()
+        {
+            var message = _queue.Receive(new TimeSpan(0, 0, 1));
+            if (message != null)
+            {
+                message.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+                return message.Body.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
