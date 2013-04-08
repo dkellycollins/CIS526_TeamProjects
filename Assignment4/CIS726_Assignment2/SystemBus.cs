4b5c15fbfe6761bde6ccc36a87801467df856b41
kcducttaper <kcducttaper@gmail.com>
2013-04-07 19:08:21 -0500

    Update SystemBus.cs

--- Assignment4/CIS726_Assignment2/SystemBus.cs
+++ Assignment4/CIS726_Assignment2/SystemBus.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,25 +6,97 @@ using System.Messaging;
 
 namespace CIS726_Assignment2
 {
+
     public class SystemBus
     {
         private MessageQueue _queue;
+
+        const string queueName = @".\private$\Assignment4Queue";
 
         public SystemBus(string queueName)
         {
             _queue = new MessageQueue(queueName);
         }
 
-        public void SendMessage(string label, string message)
+        public void SendMessage<T>(T message)
+        {            
+             MessageQueue msMq = null;
+
+            if (!MessageQueue.Exists(queueName))
+                {
+                   msMq = MessageQueue.Create(queueName);
+                }
+            else
+                {
+                    msMq = new MessageQueue(queueName);
+                }
+        try
+        {        
+            msMq.Send(message);
+        }
+        catch (MessageQueueException ee)
         {
+            Console.Write(ee.ToString());
+        }
+        catch (Exception eee)
+        {
+            Console.Write(eee.ToString());
+        }
+        finally
+        {
+            msMq.Close();
+        }
+            Console.WriteLine("Message sent ......");
+             
+            /*
             Message msg = new Message();
             msg.Body = message;
             msg.Label = label;
             _queue.Send(msg);
+             * */
         }
 
-        public string CheckMessage()
+        public void CheckMessage<T>()
         {
+            MessageQueue msMq = msMq = new MessageQueue(queueName);
+            
+            try
+            {
+
+                msMq.Formatter = new XmlMessageFormatter(new Type[] { typeof(T) });
+
+                var message = (T)msMq.Receive().Body;
+
+                //Console.WriteLine("FirstName: " + message.FirstName + ", LastName: " + message.LastName);
+
+                // Console.WriteLine(message.Body.ToString());
+
+            }
+
+            catch (MessageQueueException ee)
+            {
+
+                Console.Write(ee.ToString());
+
+            }
+
+            catch (Exception eee)
+            {
+
+                Console.Write(eee.ToString());
+
+            }
+
+            finally
+            {
+
+                msMq.Close();
+
+            }
+
+            Console.WriteLine("Message received ......");
+
+            /*
             var message = _queue.Receive(new TimeSpan(0, 0, 1));
             if (message != null)
             {
@@ -35,6 +107,7 @@ namespace CIS726_Assignment2
             {
                 return null;
             }
+             * */
         }
     }
 }
