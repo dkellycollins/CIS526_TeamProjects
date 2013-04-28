7238d58d87587d8eb5816bb6e094699977a59caa
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-04-28 16:20:39 -0500

    Implemented Threading in the database projects.

--- Assignment5/AuthParser/Program.cs
+++ Assignment5/AuthParser/Program.cs
@@ -11,52 +11,69 @@ using AuthParser.Models;
 using System.Windows.Forms;
 
 using System.Windows.Forms;
+using System.Threading;
 
 namespace AuthParser
 {
     class Program
     {
+        static AccountDBContext context;
+        static ObjectMessageQueue queue;
+
         static void Main(string[] args)
         {
             AppDomain.CurrentDomain.SetData("DataDirectory", Application.StartupPath);
 
-            AccountDBContext context = new AccountDBContext();
+            context = new AccountDBContext();
 
-            ObjectMessageQueue queue = new ObjectMessageQueue();
             ObjectMessageQueue.InitializeQueue(ObjectMessageQueue.AUTH_REQUEST);
+            queue = new ObjectMessageQueue();
+
             while (true)
             {
                 try
                 {
                     Object obj = queue.receiveObject(ObjectMessageQueue.AUTH_REQUEST);
-                    GenericRequest gen_req = obj as GenericRequest;
-
-                    if (gen_req == null)
-                    {
-                        Console.WriteLine("Error processing request, it is not a request object!");
-                        queue.sendObject(null, ObjectMessageQueue.AUTH_RESPONSE);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Fetching data...");
-                        AuthProcessor parser = new AuthProcessor(context, gen_req);
-                        try
-                        {
-                            Object result = typeof(AuthProcessor).GetMethod(gen_req.Method.ToString()).Invoke(parser, null);
-                            Console.WriteLine("It worked!");
-                            queue.sendResponse(result, ObjectMessageQueue.AUTH_RESPONSE);
-                        }
-                        catch (SqlException)
-                        {
-                            Console.WriteLine("An error occurred.");
-                        }
-                    }
+                    new Thread(ProcessRequest).Start(obj);
+                    //ProcessRequest(obj);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.ToString());
                     continue;
                 }
+            }
+        }
+
+        static void ProcessRequest(object obj)
+        {
+            try
+            {
+                GenericRequest gen_req = obj as GenericRequest;
+                if (gen_req == null)
+                {
+                    Console.WriteLine("Error processing request, it is not a request object!");
+                    queue.sendObject(null, ObjectMessageQueue.AUTH_RESPONSE);
+                }
+                else
+                {
+                    Console.WriteLine("Fetching data...");
+                    AuthProcessor parser = new AuthProcessor(context, gen_req);
+                    try
+                    {
+                        Object result = typeof(AuthProcessor).GetMethod(gen_req.Method.ToString()).Invoke(parser, null);
+                        Console.WriteLine("It worked!");
+                        queue.sendResponse(result, ObjectMessageQueue.AUTH_RESPONSE);
+                    }
+                    catch (SqlException)
+                    {
+                        Console.WriteLine("An error occurred.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
             }
         }
     }
--- Assignment5/MessageParser/Program.cs
+++ Assignment5/MessageParser/Program.cs
@@ -10,53 +10,69 @@ using MessageParser.Processor;
 using System.IO;
 
 using System.Windows.Forms;
+using System.Threading;
 
 namespace MessageParser
 {
     class Program
     {
+        static CourseDBContext context;
+        static ObjectMessageQueue queue;
+
         static void Main(string[] args)
         {
             AppDomain.CurrentDomain.SetData("DataDirectory", Application.StartupPath);
 
-            CourseDBContext context = new CourseDBContext();
-            
+            context = new CourseDBContext();
+
             ObjectMessageQueue.InitializeQueue(ObjectMessageQueue.DB_REQUEST);
-            ObjectMessageQueue queue = new ObjectMessageQueue();
+            queue = new ObjectMessageQueue();
 
             while (true)
             {
                 try
                 {
                     Object obj = queue.receiveObject(ObjectMessageQueue.DB_REQUEST);
-                    GenericRequest gen_req = obj as GenericRequest;
-                    
-                    if (gen_req == null)
-                    {
-                        Console.WriteLine("Error processing request, it is not a request object!");
-                        queue.sendObject(null, ObjectMessageQueue.DB_RESPONSE);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Fetching data...");
-                        MessageProcessor parser = new MessageProcessor(context, gen_req);
-                        try
-                        {
-                            Object result = typeof(MessageProcessor).GetMethod(gen_req.Method.ToString()).Invoke(parser, null);
-                            Console.WriteLine("It worked!");
-                            queue.sendResponse(result, ObjectMessageQueue.DB_RESPONSE);
-                        }
-                        catch (SqlException)
-                        {
-                            Console.WriteLine("An error occurred.");
-                        }
-                    }
+                    new Thread(ProcessRequest).Start(obj);
+                    //ProcessRequest(obj);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.ToString());
                     continue;
                 }
+            }
+        }
+
+        static void ProcessRequest(object obj)
+        {
+            try
+            {
+                GenericRequest gen_req = obj as GenericRequest;
+                if (gen_req == null)
+                {
+                    Console.WriteLine("Error processing request, it is not a request object!");
+                    queue.sendObject(null, ObjectMessageQueue.DB_RESPONSE);
+                }
+                else
+                {
+                    Console.WriteLine("Fetching data...");
+                    MessageProcessor parser = new MessageProcessor(context, gen_req);
+                    try
+                    {
+                        Object result = typeof(MessageProcessor).GetMethod(gen_req.Method.ToString()).Invoke(parser, null);
+                        Console.WriteLine("It worked!");
+                        queue.sendResponse(result, ObjectMessageQueue.DB_RESPONSE);
+                    }
+                    catch (SqlException)
+                    {
+                        Console.WriteLine("An error occurred.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
             }
         }
     }
