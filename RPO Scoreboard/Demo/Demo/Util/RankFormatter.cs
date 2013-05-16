2740003ac6b7a3c51e136985e7300363838eabb4
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-05-16 00:30:48 -0500

    The table is looking more like how the library wants it.

---
+++ RPO Scoreboard/Demo/Demo/Util/RankFormatter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Util
+{
+    public class RankFormatter
+    {
+        public static string Format(int rank)
+        {
+            int mod10 = rank % 10;
+            int mod100 = rank % 100;
+            string suffix;
+            if (mod100 == 11 || mod100 == 12 || mod100 == 13)
+            {
+                suffix = "th";
+            }
+            else
+            {
+                switch (mod10)
+                {
+                    case 1:
+                        suffix = "st";
+                        break;
+                    case 2:
+                        suffix = "nd";
+                        break;
+                    case 3:
+                        suffix = "rd";
+                        break;
+                    default:
+                        suffix = "th";
+                        break;
+                }
+            }
+
+            return rank + suffix;
+        }
+    }
+}
